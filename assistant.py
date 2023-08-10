8aa78fa8357c5e6e120a7ad01a21ddee8e26d7e4
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2023-08-10 20:55:53 +0800

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -53,6 +53,10 @@ def get_search_data(search_term):
 
     return_string = ""
 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+
     # Get video data
 
     # Request payload (data you want to send in the POST request)
@@ -61,7 +65,7 @@ def get_search_data(search_term):
     }
 
     # Sending the POST request
-    response = requests.post(videos_search_url, json=data)
+    response = requests.post(videos_search_url, json=data, headers=headers)
 
     # Extracting the response data
     return_string += "<h4>Here are some videos that might be helpful:<br></h4>"
@@ -85,7 +89,7 @@ def get_search_data(search_term):
     }
 
     # Sending the POST request
-    response = requests.post(notes_search_url, json=data)
+    response = requests.post(notes_search_url, json=data, headers=headers)
 
     return_string += "<h4>Here are some content from the lecture notes that might be helpful:<br></h4>"
 
